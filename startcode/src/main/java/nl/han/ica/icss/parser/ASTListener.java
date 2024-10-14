f685bdfdcd0dff58bc1e23ee9601d946085145d8
Haroon Muhammad <HaroonNisar@student.han.nl>
2024-10-14 11:51:26 +0200

    Checker start
    Listener lvl 2 nog steeds niet werkend
    checker start toegevoegd

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -89,25 +89,34 @@ public class ASTListener extends ICSSBaseListener {
 
     @Override
     public void enterValue(ICSSParser.ValueContext ctx) {
+        Expression expression = null;
         if (ctx.COLOR() != null) {
-            Expression colorLiteral = new ColorLiteral(ctx.COLOR().getText());
-            currentContainer.push(colorLiteral);
+            expression = new ColorLiteral(ctx.COLOR().getText());
         } else if (ctx.PIXELSIZE() != null) {
-            Expression pixelLiteral = new PixelLiteral(ctx.PIXELSIZE().getText());
-            currentContainer.push(pixelLiteral);
+            expression = new PixelLiteral(ctx.PIXELSIZE().getText());
         } else if (ctx.PERCENTAGE() != null) {
-            Expression percentageLiteral = new PercentageLiteral(ctx.PERCENTAGE().getText());
-            currentContainer.push(percentageLiteral);
+            expression = new PercentageLiteral(ctx.PERCENTAGE().getText());
+        } else if (ctx.SCALAR() != null) {
+            expression = new ScalarLiteral(ctx.SCALAR().getText());
         } else if (ctx.TRUE() != null || ctx.FALSE() != null) {
-            Expression boolLiteral = new BoolLiteral(ctx.getText());
-            currentContainer.push(boolLiteral);
+            expression = new BoolLiteral(ctx.getText());
+        }
+        if (expression != null) {
+            currentContainer.push(expression);
         }
     }
 
     @Override
     public void exitValue(ICSSParser.ValueContext ctx) {
-        Expression value = (Expression) currentContainer.pop();
-        currentContainer.peek().addChild(value);
+        if (!currentContainer.isEmpty() && currentContainer.peek() instanceof Expression) {
+            Expression value = (Expression) currentContainer.pop();
+            if (!currentContainer.isEmpty() && currentContainer.peek() instanceof Declaration) {
+                Declaration declaration = (Declaration) currentContainer.peek();
+                declaration.expression = value;
+            } else {
+                currentContainer.peek().addChild(value);
+            }
+        }
     }
 
     @Override
@@ -134,18 +143,18 @@ public class ASTListener extends ICSSBaseListener {
         currentContainer.peek().addChild(propertyName);
     }
 
-
     @Override
     public void enterOperator(ICSSParser.OperatorContext ctx) {
+        Operation operation = null;
         if (ctx.PLUS() != null) {
-            AddOperation addOperation = new AddOperation();
-            currentContainer.push(addOperation);
+            operation = new AddOperation();
         } else if (ctx.MIN() != null) {
-            SubtractOperation subtractOperation = new SubtractOperation();
-            currentContainer.push(subtractOperation);
+            operation = new SubtractOperation();
         } else if (ctx.MUL() != null) {
-            MultiplyOperation multiplyOperation = new MultiplyOperation();
-            currentContainer.push(multiplyOperation);
+            operation = new MultiplyOperation();
+        }
+        if (operation != null) {
+            currentContainer.push(operation);
         }
     }
 
@@ -154,52 +163,4 @@ public class ASTListener extends ICSSBaseListener {
         Operation operation = (Operation) currentContainer.pop();
         currentContainer.peek().addChild(operation);
     }
-
-//    @Override
-//    public void enterEveryRule(ParserRuleContext ctx) {
-//        super.enterEveryRule(ctx);
-//    } moet dit????
-
-    //    @Override
-//    public void enterValue(ICSSParser.ValueContext ctx) {
-//        Value value = new Value();
-//        Value value = (Value) currentContainer.peek();
-//        if (ctx.COLOR() != null) {
-//            value = new ColorLiteral(ctx.COLOR().getText());
-//        } else if (ctx.PIXELSIZE() != null) {
-//            value = new value(ctx.PIXELSIZE().getText());
-//        } else if (ctx.PERCENTAGE() != null) {
-//            value = new PercentageLiteral(ctx.PERCENTAGE().getText());
-//        } else if (ctx.BOOL() != null) {
-//            value = new BoolLiteral(ctx.BOOL().getText());
-//        } else if (ctx.VARIABLE() != null) {
-//            value = new VariableReference(ctx.VARIABLE().getText());
-//        }
-//    }
-
-    //    @Override
-//    public void enterSelector(ICSSParser.SelectorContext ctx) {
-////        Selector selector = new Selector(ctx.getText());
-//        Selector selector = new Selector("TAG");
-//        currentContainer.push(selector);
-//    }
-//
-//    @Override
-//    public void exitSelector(ICSSParser.SelectorContext ctx) {
-//        Selector selector = (Selector) currentContainer.pop();
-//        currentContainer.peek().addChild(selector);
-//    }
-
-//    @Override
-//    public void enterTagSelecter(ICSSParser.TagSelecterContext ctx) {
-//        TagSelector tagSelector = new TagSelector(ctx.getText());
-//        currentContainer.push(tagSelector);
-//    }
-//
-//    @Override
-//    public void exitTagSelecter(ICSSParser.TagSelecterContext ctx) {
-//        TagSelector tagSelector = (TagSelector) currentContainer.pop();
-//        currentContainer.peek().addChild(tagSelector);
-//    }
-
 }
